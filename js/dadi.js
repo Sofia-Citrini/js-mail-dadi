8efc263e9500f1622dfb7951812fa5238926a7dc
Sofia-Citrini <sofiacitrini@gmail.com>
2022-11-03 20:10:12 +0100

    completed dice exercise

--- js/dadi.js
+++ js/dadi.js
@@ -1,13 +1,37 @@
-// numeri random
-const numGiocatore = Math.ceil(Math.random() * 6);
-const numComputer = Math.ceil(Math.random() * 6);
+const btnGenera = document.getElementById ("genera");
 
-// computer e giocatore
-const giocatore = document.getElementById("giocatore");
-giocatore.innerHTML = `Giocatore: ${numGiocatore}`;
+btnGenera.addEventListener("click", function() {
+    // Genera numeri
+    const numGiocatore = Math.ceil(Math.random() * 6);
+    const numComputer = Math.ceil(Math.random() * 6);
 
-const computer = document.getElementById("computer");
-computer.innerHTML = `Computer: ${numComputer}`;
+    // computer e giocatore
+    const giocatore = document.getElementById("giocatore");
+    giocatore.innerHTML +=` ${numGiocatore}`;
+    giocatore.classList.add ("fw-bold", "fs-5");
 
+    const computer = document.getElementById("computer");
+    computer.innerHTML +=` ${numComputer}`;
+    computer.classList.add ("fw-bold", "fs-5");
 
+    // vincitore con numero più alto
+    const risultato = document.querySelector (".risultato")
 
+    let color = ""
+
+    if (numGiocatore < numComputer) {
+        color="text-primary";
+        risultato.innerHTML = `<div class="${color}">Computer ha vinto</div>`;
+    } else if (numGiocatore === numComputer){
+        color ="text-secondary";
+        risultato.innerHTML = `<div class="${color}">Pareggio</div>`;
+    } else {
+        color="text-success";
+        risultato.innerHTML = `<div class="${color}">Giocatore ha vinto</div>`;
+    } 
+})
+
+const btnRefresh = document.getElementById ("refresh");
+btnRefresh.addEventListener("click", function() {
+    location.reload();
+})
