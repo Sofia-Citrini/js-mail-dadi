ada15f32c77747b967736cf56f648073e3ab0306
Sofia-Citrini <sofiacitrini@gmail.com>
2022-11-03 18:37:50 +0100

    add input and bootstrap

--- js/main.js
+++ js/main.js
@@ -1,29 +1,32 @@
 // Chiedere la mail 
-const mail = prompt("Inserisci la tua e-mail");
-console.log = (`Email utente: ${mail}`);
-
-const mailUtente = document.querySelector(".mail-utente")
-mailUtente.innerHTML = `Email utente: ${mail}`;
+const mailUtente = document.querySelector("[name='mail-utente']");
+const btnControlla = document.getElementById("controlla");
 
 // Controllare la mail 
-const mailList = ["mariorossi@gmail.com", "annaverdi@gmail.com", "pietrobianchi@gmail.com"];
-console.log = (mailList);
+btnControlla.addEventListener("click", function () {
+    const mailList = ["mariorossi@gmail.com", "annaverdi@gmail.com", "pietrobianchi@gmail.com"];
+    console.log = (mailList);
 
-let utente = false;
+    let utente = false;
 
-for (let i = 0; i < mailList.length; i++) {
+    for (let i = 0; i < mailList.length; i++) {
 
-    if (mailList[i] === mail) {
-        utente = true;
+        if (mailList[i] === mailUtente.value) {
+            utente = true;
+        }
     }
-}
 
-// messagio di mail esistente o no
-const mailStato = document.querySelector(".stato")
+    // messagio di mail esistente o no
+    const mailStato = document.querySelector(".stato")
 
-if (utente === true) {
-    console.log = "E-mail approvata";
-    mailStato.innerHTML = `email approvata`;
-} else {
-    mailStato.innerHTML = `email errata`;
-}
+    if (utente === true) {
+        console.log = "E-mail approvata";
+        mailStato.innerHTML = `email approvata`;
+        mailStato.classList.add ("text-success");
+    } else {
+        mailStato.innerHTML = `email errata. riprova!`;
+        mailStato.classList.add ("text-danger");
+    }
+})
+
+
